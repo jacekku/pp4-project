68c4375fc0960238760b535d26d7210ccb2a7717
jacekku <jacekwyroba97@gmail.com>
2019-02-22 20:11:34 +0100

    fixed to local file

--- back/app.js
+++ back/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT||5000
-const postgres = require('test.js')
+const postgres = require('./test.js')
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/postgres', (req, res) => res.send(postgres.getComments()))
 
