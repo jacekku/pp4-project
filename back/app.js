7c74ffd1a4ed21fda6a4c903618bfb182dd3c268
jacekku <jacekwyroba97@gmail.com>
2019-02-22 20:09:49 +0100

    added hook

--- back/app.js
+++ back/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT||5000
-
+const postgres = require('test.js')
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/postgres', (req, res) => res.send(postgres.getComments()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
