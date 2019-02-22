3dc0be770a5c525c2b6e43dfed0e21daf8a45b97
jacekku <jacekwyroba97@gmail.com>
2019-02-22 21:53:30 +0100

    ...

--- back/app.js
+++ back/app.js
@@ -3,8 +3,8 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT||5000
 const postgres = require('./test.js')
-
+app.use(cors())
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
 app.get('/postgres', postgres.getComments)
-app.use(cors())
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
