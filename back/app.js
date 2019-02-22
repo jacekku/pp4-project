72f068cc3727662d273d479fe22bcd5fc6e4c222
jacekku <jacekwyroba97@gmail.com>
2019-02-22 21:42:54 +0100

    checking if html tags can be sent

--- back/app.js
+++ back/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT||5000
 const postgres = require('./test.js')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
 app.get('/postgres', postgres.getComments)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
