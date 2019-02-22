fee9b7b847126ff60eba52c25df94c2430affb0a
jacekku <jacekwyroba97@gmail.com>
2019-02-22 22:54:32 +0100

    added body parser

--- back/app.js
+++ back/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+var bodyParser = require('body-parser')
+
 const app = express()
 const port = process.env.PORT||5000
 const postgres = require('./test.js')
 app.use(cors())
+app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
-app.get('/postgres', postgres.getComments)
+app.get('/postgres', postgres.getMessages)
+app.post('/postmessage', postgres.postMessage)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
