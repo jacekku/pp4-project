3617d0e71ae2d5bc627b34539f95abfc0ebae0c1
jacekku <jacekwyroba97@gmail.com>
2019-02-23 22:20:14 +0100

    added google auth

--- front/validateForm.js
+++ front/validateForm.js
@@ -30,6 +30,16 @@ function validate(e){
 function clearChatBox(){
     chatBox.innerHTML=""
 }
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+
+
 function refreshChatBox(){
     clearChatBox()
     getComments()
