475789cd140b9786087b4ef68ab4b9ac00cbf093
jacekku <jacekwyroba97@gmail.com>
2019-02-23 22:28:26 +0100

    added sign out

--- front/validateForm.js
+++ front/validateForm.js
@@ -30,6 +30,12 @@ function validate(e){
 function clearChatBox(){
     chatBox.innerHTML=""
 }
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
