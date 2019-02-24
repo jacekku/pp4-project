c0279fe90df9fad36dd7d91e85e59f031469848b
jacekku <jacekwyroba97@gmail.com>
2019-02-24 22:21:22 +0100

    renamed to router and registering implemented registering

---
+++ back/router.js
@@ -0,0 +1,36 @@
+const {
+  Client
+} = require('pg');
+const passwordManage = require('./handlePasswords')
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+client.connect();
+
+function getMessages(request, response) {
+  client.query('SELECT * from comments order by created_on asc;', (err, result) => {
+    if (err) throw err;
+    response.status(200).json(result.rows)
+  });
+
+}
+function postMessage(request,response){
+  response.status(501)
+}
+function register(request,response){
+  const data = request.headers.authorize
+  const text = Buffer.from(data, 'base64').toString('ascii')
+  const {nickname,passwordUnhashed} = JSON.parse(text)
+  const {password,salt} = passwordManage.saltHashPassword(passwordUnhashed)
+  client.query(`INSERT INTO users (nickname,password,salt) VALUES (${nickname},${password},${salt})`)
+  response.status(201)
+
+}
+module.exports = {
+  getMessages,
+  postMessage,
+  register
+};
+
