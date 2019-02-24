ad0f2182fa841d82d2b40483d55937d091802585
jacekku <jacekwyroba97@gmail.com>
2019-02-24 22:27:12 +0100

    more logging

--- back/router.js
+++ back/router.js
@@ -23,6 +23,7 @@ function register(request,response){
   const data = request.headers.authorize
   const text = Buffer.from(data, 'base64').toString('ascii')
   const {nickname,passwordUnhashed} = JSON.parse(text)
+  console.log(`data (${nickname},${passwordUnhashed})`)
   const {password,salt} = passwordManage.saltHashPassword(passwordUnhashed)
   console.log(`inserting (${nickname},${password},${salt})`)
   client.query(`INSERT INTO users (nickname,password,salt) VALUES (${nickname},${password},${salt})`)
