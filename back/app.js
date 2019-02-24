df41956515f33c99a6deab6cb3ed2d5a1ed05075
jacekku <jacekwyroba97@gmail.com>
2019-02-24 22:20:56 +0100

    added routing

--- back/app.js
+++ back/app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const router = require('./router.js/index.js')
 
 const app = express()
 const port = process.env.PORT||5000
-const postgres = require('./test.js')
+
 app.use(cors({methods:"GET,POST"}))
 app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
-app.get('/postgres', postgres.getMessages)
-app.post('/postmessage', postgres.postMessage)
+app.get('/postgres', router.getMessages)
+// app.post('/postmessage', router.postMessage)
+app.post('/register', router.register)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
