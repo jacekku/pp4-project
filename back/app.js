f582b39a958f7e225e55e68f45c1404854645dbd
jacekku <jacekwyroba97@gmail.com>
2019-02-24 22:22:42 +0100

    fixing worng path

--- back/app.js
+++ back/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const router = require('./router.js/index.js')
+const router = require('./router.js')
 
 const app = express()
 const port = process.env.PORT||5000
