dd13f7c5a0eb3d3dbac4613214cb7ac93e4b1bdb
jacekku <jacekwyroba97@gmail.com>
2019-02-25 21:47:29 +0100

    implemented getToken

--- front/global.js
+++ front/global.js
@@ -1,8 +1,22 @@
 async function alreadyRegistered(nick) {
-    let res=await fetch(`https://pp4-project.herokuapp.com/user/${nick}`)
+    let res = await fetch(`https://pp4-project.herokuapp.com/user/${nick}`)
     let js
-    await res.json().then(r=>js=r)
-    if(js.length>0)return true
-    if(res.status==500)console.error("internal server error")
+    await res.json().then(r => js = r)
+    if (js.length > 0) return true
+    if (res.status == 500) console.error("internal server error")
     return false
+}
+async function getSessionToken(nickname, token, justRegistered) {
+    if (token == null && !justRegistered) {
+        return false
+    }
+    let t = await fetch('https://pp4-project.herokuapp.com/token', {
+        headers: {
+            "Authorize": btoa(JSON.stringify({
+                "nickname": nickname,
+                "token": token
+            }))
+        }
+    })
+    console.log(t)
 }
