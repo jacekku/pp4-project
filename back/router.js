ee6c391b6a3984458148809bf0d9c423a8fc019a
jacekku <jacekwyroba97@gmail.com>
2019-02-25 18:57:35 +0100

    fixed unclosed })

--- back/router.js
+++ back/router.js
@@ -19,47 +19,39 @@ function getMessages(request, response) {
   });
 
 }
-
-function postMessage(request, response) {
+function postMessage(request,response){
   response.sendStatus(501)
 }
-
-function register(request, response) {
+function register(request,response){
   const data = request.headers.authorize
   const text = Buffer.from(data, 'base64').toString('ascii')
-  const {
-    nickname,
-    passwordUnhashed
-  } = JSON.parse(text)
-  const {
-    password,
-    salt
-  } = passwordManage.saltHashPassword(passwordUnhashed)
+  const {nickname,passwordUnhashed} = JSON.parse(text)
+  const {password,salt} = passwordManage.saltHashPassword(passwordUnhashed)
   client.query(`SELECT nickname FROM users where nickname = '${nickname}'`,
+  (err, result) => {
+    if (err){
+      response.sendStatus(500)
+      console.error(err)
+      return false
+    }
+    if(result.rows.length!=0){
+      response.sendStatus(409)
+      return false
+    }else{
+      client.query(`INSERT INTO users (nickname,password,salt) VALUES ('${nickname}','${password}','${salt}')`,
     (err, result) => {
-      if (err) {
+      if (err){
         response.sendStatus(500)
         console.error(err)
         return false
       }
-      if (result.rows.length != 0) {
-        response.sendStatus(409)
-        return false
-      } else {
-        client.query(`INSERT INTO users (nickname,password,salt) VALUES ('${nickname}','${password}','${salt}')`,
-          (err, result) => {
-            if (err) {
-              response.sendStatus(500)
-              console.error(err)
-              return false
-            }
-            response.sendStatus(201)
-          });
-      }
+      response.sendStatus(201)
+    });
     }
-  )
-
-function getNickname(request, response) {
+  })
+  
+  }
+function getNickname(request,response){
   const nickname = request.params.nickname
   client.query(`SELECT nickname FROM users where nickname = '${nickname}'`,
     (err, result) => {
@@ -67,34 +59,31 @@ function getNickname(request, response) {
         response.sendStatus(500)
         console.error(err)
       }
-      if (result.rows.length == 0) {
+      if(result.rows.length==0){
         response.sendStatus(404)
       }
       response.status(200).json(result.rows)
     });
 }
 
-function checkLogin(request, response) {
+function checkLogin(request,response){
   const data = request.headers.authorize
   const text = Buffer.from(data, 'base64').toString('ascii')
-  const {
-    nickname,
-    passwordUnhashed
-  } = JSON.parse(text)
+  const {nickname,passwordUnhashed} = JSON.parse(text)
   client.query(`SELECT password,salt FROM users where nickname = ${nickname}`,
-    (err, result) => {
-      if (err) {
-        response.sendStatus(500)
-        console.error(err)
-      }
-      console.log(result.rows)
-      res = passwordManage.checkPassword(passwordUnhashed, result.rows.password, result.rows.salt)
-      if (res) {
-        response.sendStatus(200)
-      } else {
-        response.sendStatus(401)
-      }
-    })
+  (err,result)=>{
+    if (err) {
+      response.sendStatus(500)
+      console.error(err)
+    }
+    console.log(result.rows)
+    res = passwordManage.checkPassword(passwordUnhashed,result.rows.password,result.rows.salt)
+    if(res){
+      response.sendStatus(200)
+    }else{
+      response.sendStatus(401)
+    }
+  })
 }
 
 module.exports = {
@@ -103,4 +92,5 @@ module.exports = {
   register,
   getNickname,
   checkLogin
-}
+};
+
