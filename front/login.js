5c876488d566b7e109e34d87e2d2c66e47c0a2d5
jacekku <jacekwyroba97@gmail.com>
2019-02-25 20:33:13 +0100

    some more loggin logic

--- front/login.js
+++ front/login.js
@@ -1,37 +1,40 @@
-  
-window.addEventListener('load', onload)
+  window.addEventListener('load', onload)
 
-function onload() {
-    form = document.getElementById('login-form')
-    form.addEventListener('submit',val)
-    feedback=document.getElementById('feedback')
-}
-async function val(e){
-    e.preventDefault()
-    const nickname = e.target[0].value
-    const password = e.target[1].value
-    let reg = await alreadyRegistered(nickname)
-    if(reg){await login(nickname,password)}
-    else {feedback.innerHTML='wrong user or password'}
-}
+  function onload() {
+      form = document.getElementById('login-form')
+      form.addEventListener('submit', val)
+      feedback = document.getElementById('feedback')
+  }
+  async function val(e) {
+      e.preventDefault()
+      const nickname = e.target[0].value
+      const password = e.target[1].value
+      let reg = await alreadyRegistered(nickname)
+      if (reg) {
+          let loggedIn = await login(nickname, password)
+          if (loggedIn) {
+              feedback.innerHTML = "<font color='green'>Logged in</font>"
+          } else {
+              feedback.innerHTML = "<font color='red'>Wrong user or password</font>"
+          }
+      } else {
+          feedback.innerHTML = "<font color='red'>Wrong user or password</font>"
+      }
+  }
 
 
-
-function login(nick,pass) {
-    fetch(`https://pp4-project.herokuapp.com/login`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Authorize": btoa(JSON.stringify({
-                "nickname": nick,
-                "passwordUnhashed": pass
-            })),
-            "Access-Control-Allow-Origin": "*"
-        },
-    }).then(response => {
-        console.info(response)
-    }).catch(response => {
-        console.info(response)
-    })
-    return true
-}
+  async function login(nick, pass) {
+      let f=await fetch(`https://pp4-project.herokuapp.com/login`, {
+          method: 'POST',
+          headers: {
+              "Content-Type": "application/json",
+              "Authorize": btoa(JSON.stringify({
+                  "nickname": nick,
+                  "passwordUnhashed": pass
+              })),
+              "Access-Control-Allow-Origin": "*"
+          },
+      })
+      if(f.status==200)return true
+      return false
+  }
