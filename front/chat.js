9499281fcfa6ca295a38b8f24b135b3edd0caef9
jacekku <jacekwyroba97@gmail.com>
2019-02-26 19:38:20 +0100

    message drawing handler

--- front/chat.js
+++ front/chat.js
@@ -1,20 +1,23 @@
+let textarea, messages_div, last_message
 window.addEventListener('load', () => {
     greet = document.getElementById('greet')
     greet.innerHTML += `${localStorage.getItem('user')}!`
-    var textarea = document.getElementById('new-message')
-    textarea.value=''
-    textarea.addEventListener('keyup',e=>{
-        if(e.keyCode==13){
-            if(textarea.value!=""){
-                let textVal=textarea.value
+    textarea = document.getElementById('new-message')
+    textarea.value = ''
+    textarea.addEventListener('keyup', e => {
+        if (e.keyCode == 13) {
+            if (textarea.value != "") {
+                let textVal = textarea.value
                     .trim()
-                    .replace("\n$","")
+                    .replace("\n$", "")
                 newMessage(textVal)
             }
-            textarea.value=''
+            textarea.value = ''
         }
     })
-    var messages_div = document.getElementById('messages')
+    messages_div = document.getElementById('messages')
+    handleMessages()
+    setInterval(handleMessages, 2500)
 })
 
 
@@ -24,11 +27,11 @@ function logout() {
     localStorage.removeItem('user')
 }
 
-async function getMessages(id=-1) {
+async function getMessages(id = -1) {
     let out
     await fetch(`https://pp4-project.herokuapp.com/messages/${id}`)
         .then(res => {
-            res.json().then(r=>out=r)
+            res.json().then(r => out = r)
         }).catch(err => {
             console.info(err)
         })
@@ -54,4 +57,33 @@ async function newMessage(message) {
     }).catch(response => {
         console.info(response)
     })
+    handleMessages()
+}
+
+
+function getDiv(message_obj) {
+    const {
+        nickname,
+        message_date,
+        message_text
+    } = message_obj
+    const formated_date = new Date(message_date).toLocaleString()
+    const div = document.createElement('div')
+    div.innerHTML =
+        `<div class='message'>
+    <font color='#aaa'>(${formated_date})</font><font color='#666'>${nickname}</font>:${message_text}
+    </div>`
+    return div
+}
+
+function appendMessages(messages) {
+    messages.forEach(msg => messages_div.appendChild(getDiv(msg)))
+}
+async function handleMessages() {
+    const messages = await getMessages(last_message)
+    if (messages.length>0) {
+        last_message = messages[messages.length - 1].message_id || -1
+        // localStorage.setItem('last_message_id',last_message)
+        appendMessages(messages)
+    }
 }
