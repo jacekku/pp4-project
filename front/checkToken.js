3d53d024af7e38b66fbff1d3efd9ed17112f8731
jacekku <jacekwyroba97@gmail.com>
2019-02-26 12:09:51 +0100

    added token verification

--- front/checkToken.js
+++ front/checkToken.js
@@ -1,3 +1,12 @@
-window.addEventListener('load',()=>{
-    
+window.addEventListener('load',async ()=>{
+    const token = localStorage.getItem('token')
+    const user = localStorage.getItem('user')
+    if(!token){
+        window.location.href='..'
+        return false
+    }
+    if(!await checkToken(user, token)){
+        window.location.href='..'
+        return false
+    }
 })
